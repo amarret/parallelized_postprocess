237446892790e17ea23dd399a20f488cf57616d4
Alexis <alexis@Alexis>
2020-04-03 10:25:37 +0200

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,24 +1,44 @@
 
-import time
+import time as ti
 import numpy as np
 import multiprocessing
 import sys
 
-#==========================================
-#wrapper for parallelized  calculation    
-    
-def wrapper_myfunction(args):
-    
-    return myfunction(*args)
 
 
 #==========================================
 def myfunction(x,y,z):
     
-    time.sleep(1)
+    ti.sleep(1)
     
     return 3*x, y**2, z+1
 
+#==========================================
+#Parallelized computation
+def wrapper(inputs) :
+    
+    func = inputs[0]
+    args = inputs[1:]
+    
+    return func(*args)
+
+#==========================================        
+def parallel(inputs, lenght, nbrCores, *outputs):
+    
+    workers = multiprocessing.Pool(processes = nbrCores)
+    results = workers.imap(wrapper,inputs)
+    
+    for i, result in enumerate(results, 0): 
+        for j, output in enumerate(outputs, 0): 
+
+            output[i] = result[j]
+            
+        sys.stderr.write('\rProgress : {0}/{1}'.format(i+1,lenght))
+         
+    workers.close()
+    workers.join()    
+
+    return 0
 
 #==========================================
 #inputs
@@ -31,29 +51,14 @@ multiplied_x = np.zeros([10,100])
 squared_y = np.zeros([10,100])
 added_z = np.zeros([10,100])
 
+nbrCores = 2
 
-#==========================================
-#Parallelized computation
+inputs = ((myfunction, x[i], y[i], z[i]) for i in xrange(len(x)))
 
-nbrCores = 4
+strt = ti.time()
+parallel(inputs, N, nbrCores, multiplied_x, squared_y, added_z)
+print ti.time()-strt
 
-
-workers = multiprocessing.Pool(processes = nbrCores)
-
-inputs = (np.array([x[i], y[i], z[i]]) for i in xrange(N))
-
-results = workers.imap(wrapper_myfunction,inputs)
-
-for i, data in enumerate(results, 0): 
-    
-    multiplied_x[i] = data[0]
-    squared_y[i]    = data[1]
-    added_z[i]      = data[2]
-    
-    sys.stderr.write('\rProgress : {0}/{1}'.format(i+1,N))
-    
-workers.close()
-workers.join()
 
 
 
