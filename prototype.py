6a18bc33ffaebea0452d429d565dc325af036a48
Alexis <alexis@Alexis>
2020-05-15 15:56:07 +0200

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -22,27 +22,34 @@ def wrapper(inputs) :
     
     return func(*args)
 
-
-#==========================================    
+#==========================================   
+#parallelizes the calculation of a function 
+#needs : the inputs data as a single array, the first element must be the object function to parallelize
+#        the lenght of the axis along which to iterate, the nbr of cores to use
+#        the arrays where to put the results  
+    
 def parallel(inputs, lenght, nbrCores, *outputs):
     
     workers = multiprocessing.Pool(processes = nbrCores)
     results = workers.imap(wrapper,inputs)
     
-    for i, result in enumerate(results):      
-        for j, output in enumerate(outputs):
+    for i, result in enumerate(results):     
+        for j, output in enumerate(outputs): 
             
             if len(result)!=len(outputs): output[i] = result
             else:                         output[i] = result[j]
             
         sys.stderr.write('\rProgress : {0}/{1}'.format(i+1,lenght))
          
-    workers.terminate()
+    workers.close()
     workers.join() 
+    workers.terminate()
     
     print ""
 
     return 0
+
+
 
 #==========================================
 #inputs
